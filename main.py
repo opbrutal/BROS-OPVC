ac9a53b275d4627283a102ca9b49805d27e47651
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-02-16 18:40:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from pytgcalls import PyTgCalls
 from pytgcalls import idle as pyidle
 from config import bot, call_py
 
+api_id = int(environ["API_ID"])
+api_hash = environ["API_HASH"]
+session = environ["SESSION"]
+group_mode = environ["GROUP_MODE"]
+hndlr = environ["HNDLR"]
+sudo_users = environ["SUDO_USERS"]
+
+plugins = dict(
+  root="ArrayCore",
+  include=[
+    "vc.player",
+    "ping",
+  ]
+)
+
 bot.start()
 print("TgVcUsers On Action Do Visit @Suzune_Support")
 call_py.start()
