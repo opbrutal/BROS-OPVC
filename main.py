9cdb5b8c1b51554d05c442ba5c9686952841911c
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-02-16 21:32:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,10 @@ from pyrogram import Client, idle
 from pytgcalls import PyTgCalls
 from pytgcalls import idle as pyidle
 from config import bot, call_py
-from os import environ 
 
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-SESSION = environ["SESSION"]
-GROUP_MODE = environ["GROUP_MODE"]
-HNDLR = environ["HNDLR"]
-SUDO_USERS = environ["SUDO_USERS"]
-
-ArrayCore = dict(
-  root="ArrayCore",
-  include=[
-    "vc.player",
-    "ping",
-  ]
-)
-
-app = Client(SESSION, API_ID, API_HASH, HNDLR, GROUP_MODE)
-app.start()
-print('Bot On Action Do Visit @Suzune_Support For Help & Support')
+bot.start()
+print("TgVcUsers On Action Do Visit @Suzune_Support")
+call_py.start()
+print("TgVcUsers Client On Action Do Visit @Suzune_Support")
+pyidle()
 idle()
-app.stop()
-print('Bot Stopped Certainly')
