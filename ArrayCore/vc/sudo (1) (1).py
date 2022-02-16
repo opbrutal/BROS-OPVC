bef5a88a73ec70acaeef298f9f93632890ad7c98
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-02-16 15:31:23 +0530

    Add files via upload

---
+++ ArrayCore/vc/sudo (1) (1).py
@@ -0,0 +1,18 @@
+import os
+
+import heroku3
+from telethon.tl.functions.users import GetFullUserRequest
+
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+sudousers = os.environ.get("SUDO_USERS", None)
+
+
+@Client.on_message(filters.command(["sudo"], prefixes=f"{HNDLR}"))
+async def sudo(event):
+    sudo = "True" if Config.SUDO_USERS else "False"
+    users = os.environ.get("SUDO_USERS", None)
+    if sudo == "True":
+        await eor(event, f"Sudo - `Enabled`\nSudo user(s) - `{users}`")
+    else:
+        await eor(event, f"**ThunderUserbot**\nSudo - `Disabled`")
