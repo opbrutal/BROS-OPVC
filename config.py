fa7cab50522a52482a098f8b6fa116b7e85d634a
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2022-02-17 06:14:55 -0800

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ API_HASH = os.getenv("API_HASH", "")
 SESSION = os.getenv("SESSION")
 HNDLR = os.getenv("HNDLR", "!")
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "")
 
                       
 contact_filter = filters.create(
