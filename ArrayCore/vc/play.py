8d6500b6fa9f4d51c0a6d35e16d4ab9e3ca0dad6
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-02 18:24:47 +0530

    Update play.py

--- ArrayCore/vc/play.py
+++ ArrayCore/vc/play.py
@@ -87,7 +87,7 @@ async def ytdl(link):
 
 @Client.on_message(filters.command(["play"], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
- if GRPPLAY or SUDO_USERS or (m.from_user and m.from_user.is_contact) or m.outgoing:
+ if GRPPLAY or 1789859817 or (m.from_user and m.from_user.is_contact) or m.outgoing:
     replied = m.reply_to_message
     chat_id = m.chat.id
     if replied:
