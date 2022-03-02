b88289873862af922e3306a686ae5ace17e8a6f1
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-02 17:41:35 +0530

    Delete funcs.py

--- funcs.py
+++
@@ -1,5 +0,0 @@
-from telegram import Chat, ChatMember
-from config import SUDO_USERS 
-
-def is_support_plus(chat: Chat, user_id: int, member: ChatMember = None) -> bool: 
-  return user_id in SUDO_USERS
