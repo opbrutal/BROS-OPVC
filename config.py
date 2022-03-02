29a0a6ef89563fa7685633cedd7ec03c06d52003
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-02 19:04:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
-
+from os import getenv
 # For Local Deploy
 if os.path.exists(".env"):
     load_dotenv(".env")
