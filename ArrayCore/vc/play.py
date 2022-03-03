9c30f28a47b038d1e90ccc175ad7f8cb023c56fa
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-03 20:48:05 +0530

    Update play.py

--- ArrayCore/vc/play.py
+++ ArrayCore/vc/play.py
@@ -86,7 +86,7 @@ async def ytdl(link):
 
 @Client.on_message(filters.command(["play"], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
-    if GRPPLAY != "True":
+    if GRPPLAY:
         if m.from_user.id not in SUDO_USERS:
             return # Pylint: DISABLE
     replied = m.reply_to_message
