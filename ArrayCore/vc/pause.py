996a0661951c61555bb8017c1dfff485b3c604fd
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-05 00:50:17 +0530

    Update pause.py

--- ArrayCore/vc/pause.py
+++ ArrayCore/vc/pause.py
@@ -13,16 +13,16 @@ for x in Var.SUDO_USERS:
 @Client.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
-    await e.delete()
-    chat_id = e.chat.id
-    if chat_id in QUEUE:
-        try:
-            await call_py.pause_stream(chat_id)
-            await e.reply(
-                f"**Paued In {chat_id}**"
-            )
-        except Exception as e:
-            await e.reply(f"**ERROR** \n`{e}`")
-    else:
-        await e.reply("**Nothing is playing!**")
+        await e.delete()
+        chat_id = e.chat.id
+        if chat_id in QUEUE:
+            try:
+                await call_py.pause_stream(chat_id)
+                await e.reply(
+                    f"**Paued In {chat_id}**"
+                )
+            except Exception as e:
+                await e.reply(f"**ERROR** \n`{e}`")
+        else:
+            await e.reply("**Nothing is playing!**")
       
