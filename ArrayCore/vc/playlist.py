e77cb2f72b652389948d3e6fb9f2e20dff03025e
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-05 01:54:18 +0530

    Update playlist.py

--- ArrayCore/vc/playlist.py
+++ ArrayCore/vc/playlist.py
@@ -13,12 +13,12 @@ for x in Var.SUDO_USERS:
 @Client.on_message(filters.command(["playlist"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
-       chat_id = m.chat.id
+       chat_id = e.chat.id
     if chat_id in QUEUE:
         chat_queue = get_queue(chat_id)
         if len(chat_queue) == 1:
-            await m.delete()
-            await m.reply(
+            await e.delete()
+            await e.reply(
                 f"**Playing In {chat_id}**",
                 disable_web_page_preview=True,
             )
@@ -30,8 +30,8 @@ async def ping(_, e: Message):
                 hmmm = chat_queue[x][2]
                 hmmmm = chat_queue[x][3]
                 QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
-            await m.reply(QUE, disable_web_page_preview=True)
+            await e.reply(QUE, disable_web_page_preview=True)
     else:
-        await m.reply("__Doesn't play anything__")
+        await e.reply("__Doesn't play anything__")
 
        
