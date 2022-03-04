b18bcfcdfcbc47e69b0c5551afa53e0a94067c57
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-04 20:00:44 +0530

    Add files via upload

---
+++ var.py
@@ -0,0 +1,11 @@
+from decouple import config
+from heroku3 import from_key
+from os import getenv
+
+class Var:
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.getenv("API_HASH", "")
+    SESSION = os.getenv("SESSION")
+    HNDLR = os.getenv("HNDLR", "!")
+    GROUP_MODE = os.getenv("GROUP_MODE", "True")
+    SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
