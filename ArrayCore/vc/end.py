a153f2a2b78506f067f2ec643bf2e3f9546f02f4
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-05 00:36:10 +0530

    Update end.py

--- ArrayCore/vc/end.py
+++ ArrayCore/vc/end.py
@@ -14,14 +14,14 @@ for x in Var.SUDO_USERS:
 @Client.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
-    await e.delete()
-    chat_id = e.chat.id
-    if chat_id in QUEUE:
-        try:
-            await call_py.leave_group_call(chat_id)
-            clear_queue(chat_id)
-            await e.reply("**End**")
-        except Exception as e:
-            await e.reply(f"**ERROR** \n`{e}`")
-    else:
-        await e.reply("**Nothing is playing !**")
+        await e.delete()
+        chat_id = e.chat.id
+        if chat_id in QUEUE:
+            try:
+                await call_py.leave_group_call(chat_id)
+                clear_queue(chat_id)
+                await e.reply("**End**")
+            except Exception as e:
+                await e.reply(f"**ERROR** \n`{e}`")
+        else:
+            await e.reply("**Nothing is playing !**")
