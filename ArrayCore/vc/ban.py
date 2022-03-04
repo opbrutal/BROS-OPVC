de3ab44826907393795b83518a5eb40cbbdc0373
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-04 20:30:49 +0530

    Update ban.py

--- ArrayCore/vc/ban.py
+++ ArrayCore/vc/ban.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 
 print("Starting.....")
 
-Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
+Riz = TelegramClient('Riz', Var.API_ID, Var.API_HASH).start(session=Var.SESSION)
 
 
 SUDO_USERS = []
