6180f1861e06e43302752ef2c99ca395c07711c5
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-05 02:01:03 +0530

    Update skip.py

--- ArrayCore/vc/skip.py
+++ ArrayCore/vc/skip.py
@@ -13,21 +13,21 @@ for x in Var.SUDO_USERS:
 @Client.on_message(filters.command(["skip"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
-        await m.delete()
-        chat_id = m.chat.id
-        if len(m.command) < 2:
+        await e.delete()
+        chat_id = e.chat.id
+        if len(e.command) < 2:
             op = await skip_current_song(chat_id)
             if op == 0:
-                await m.reply("**There's nothing in the queue to skip!**")
+                await e.reply("**There's nothing in the queue to skip!**")
         elif op == 1:
-                await m.reply("**Empty Queue, Leaving Voice Chat**")
+                await e.reply("**Empty Queue, Leaving Voice Chat**")
         else:
-                await m.reply(
+                await e.reply(
                     f"**Playing In {chat_id}**",
                     disable_web_page_preview=True,
             )
     else:
-        skip = m.text.split(None, 1)[1]
+        skip = e.text.split(None, 1)[1]
         OP = "**Removed the following songs from the Queue: -**"
         if chat_id in QUEUE:
             items = [int(x) for x in skip.split(" ") if x.isdigit()]
@@ -41,5 +41,5 @@ async def ping(_, e: Message):
                         pass
                     else:
                         OP = OP + "\n" + f"**#⃣{x}** - {hm}"
-            await m.reply(OP)        
+            await e.reply(OP)        
       
