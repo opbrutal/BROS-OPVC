5a3773dbf8a136647041ba32a06449a7021c715f
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 16:48:53 +0530

    Update playlist.py

--- ArrayCore/vc/playlist.py
+++ ArrayCore/vc/playlist.py
@@ -1,43 +1,33 @@
-from pyrogram import Client
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
+
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import QUEUE, clear_queue
-from var import Var
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
                     HNDLR, call_py, contact_filter, SUDO_USERS)
-
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
     
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["list"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-       chat_id = e.chat.id
+    inp = e.text[5:]
+    chat_ = await vcbot.get_chat(inp)
+    chat_id = chat_.id
     if chat_id in QUEUE:
         chat_queue = get_queue(chat_id)
         if len(chat_queue) == 1:
             await e.delete()
-            await e.reply(
-                f"**Playing In {chat_id}**",
-                disable_web_page_preview=True,
-            )
+            await e.reply_text(f"**Playing In {chat_.title}**")
         else:
-            QUE = f"**Playing In {chat_id}**"
+            QUE = f"**Playing In {chat_.title}**"
             l = len(chat_queue)
             for x in range(1, l):
                 hmm = chat_queue[x][0]
                 hmmm = chat_queue[x][2]
                 hmmmm = chat_queue[x][3]
                 QUE = QUE + "\n" + f"**#{x}** - [{hmm}]({hmmm}) | `{hmmmm}`\n"
-            await e.reply(QUE, disable_web_page_preview=True)
+            await e.reply_text(QUE, disable_web_page_preview=True)
     else:
-        await e.reply("__Doesn't play anything__")
-
-       
+        await e.reply_text("__Doesn't play anything__")
