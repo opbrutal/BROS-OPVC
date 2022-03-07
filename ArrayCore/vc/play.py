b2c32dcfd26571e41459923dc25fa62e823ab7f1
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 16:28:18 +0530

    Update play.py

--- ArrayCore/vc/play.py
+++ ArrayCore/vc/play.py
@@ -1,46 +1,33 @@
-
-import logging
-import re
-import os
-import sys
 import asyncio
-from telethon import TelegramClient, events
-import telethon.utils
-from telethon.tl import functions
-from telethon.tl.functions.channels import LeaveChannelRequest
+import datetime
+import logging
+import os
+import re
+import sys
+
 from asyncio import sleep
-from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins, ChatAdminRights
-from telethon.tl.functions.channels import EditBannedRequest
-from datetime import datetime
-from var import Var
-from pyrogram import Client
-from config import bot, call_py, HNDLR, contact_filter, GRPPLAY
-from pyrogram import filters
+
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from pytgcalls import StreamType
 from pytgcalls.types.input_stream import AudioPiped, AudioVideoPiped
-from pytgcalls.types.input_stream.quality import (
-    HighQualityAudio,
-    HighQualityVideo,
-    LowQualityVideo,
-    MediumQualityVideo,
-)
+from pytgcalls.types.input_stream.quality import (HighQualityAudio, HighQualityVideo,
+                                                  LowQualityVideo, MediumQualityVideo)
 from youtubesearchpython import VideosSearch
-from utils import CHAT_TITLE, gen_thumb
+
 from ArrayCore.vc.queues import QUEUE, add_to_queue, get_queue
+from utils import CHAT_TITLE, gen_thumb
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+                    HNDLR, call_py, contact_filter,
+                    SUDO_USERS, GRPPLAY)
 
 logging.basicConfig(level=logging.INFO)
 
 print("Starting.....")
 
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
 
 def ytsearch(query):
     try:
@@ -76,13 +63,14 @@ async def ytdl(link):
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["play"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-        replied = e.reply_to_message
-    chat_id = e.chat.id
+    replied = e.reply_to_message
+    inp = e.text[5:]
+    chat_ = await vcbot.get_chat(inp)
+    chat_id = chat_.id
     if replied:
         if replied.audio or replied.voice:
             await e.delete()
-            TheVenomXD = await replied.reply("**Reading Mp3.**")
+            TheVenomXD = await replied.reply_text("**Reading Mp3.**")
             dl = await replied.download()
             link = replied.link
             if replied.audio:
@@ -95,8 +83,7 @@ async def ping(_, e: Message):
             if chat_id in QUEUE:
                 pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
                 await TheVenomXD.delete()
-                caption="**Playing In {chat_id}**",
-                
+                caption="**Playing In {chat_id}**"
             else:
                 await call_py.join_group_call(
                     chat_id,
@@ -107,15 +94,13 @@ async def ping(_, e: Message):
                 )
                 add_to_queue(chat_id, songname, dl, link, "Audio", 0)
                 await TheVenomXD.delete()
-                caption="**Playing In {chat_id}**",
-                
-
+                caption="**Playing In {chat_id}**"
     else:
         if len(e.command) < 2:
-            await e.reply("Reply to Audio File or provide something for Searching ...")
+            await e.reply_text("Reply to Audio File or provide something for Searching ...")
         else:
             await e.delete()
-            TheVenomXD = await e.reply(" Searching...")
+            TheVenomXD = await e.reply_text(" Searching...")
             query = e.text.split(None, 1)[1]
             search = ytsearch(query)
             if search == 0:
@@ -127,18 +112,17 @@ async def ping(_, e: Message):
                 duration = search[2]
                 thumbnail = search[3]
                 userid = e.from_user.id
-                srrf = e.chat.title
+                srrf = chat_.title
                 ctitle = await CHAT_TITLE(srrf)
                 thumb = await gen_thumb(thumbnail, title, userid, ctitle)
                 hm, ytlink = await ytdl(url)
                 if hm == 0:
-                    await TheVenomXD.edit(f"**YTDL ERROR ️** \n\n`{ytlink}`")
+                    await TheVenomXD.edit(f"**YTDL ERROR ️!** \n\n`{ytlink}`")
                 else:
                     if chat_id in QUEUE:
                         pos = add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
                         await TheVenomXD.delete()
-                        caption=f"""**Playing In {chat_id}**""",
-                        
+                        caption=f"""**Playing In {chat_id}**"""
                     else:
                         try:
                             await call_py.join_group_call(
@@ -150,7 +134,9 @@ async def ping(_, e: Message):
                             )
                             add_to_queue(chat_id, songname, ytlink, url, "Audio", 0)
                             await TheVenomXD.delete()
-                            caption=f"""**Playing In {chat_id}**""",
-                            
+                            caption=f"""**Playing In {chat_id}**"""
                         except Exception as ep:
                             await TheVenomXD.edit(f"`{ep}`")
+
+
+# bohot errors h bc isme
