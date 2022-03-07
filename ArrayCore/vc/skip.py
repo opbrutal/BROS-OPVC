c890254309cae2f637f57410c4de4a682e607086
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 17:00:33 +0530

    Update skip.py

--- ArrayCore/vc/skip.py
+++ ArrayCore/vc/skip.py
@@ -1,36 +1,28 @@
-from pyrogram import Client
-from pyrogram import filters
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
+
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import QUEUE, clear_queue
-from var import Var
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
                     HNDLR, call_py, contact_filter, SUDO_USERS)
 
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["skip"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-        await e.delete()
-        chat_id = e.chat.id
-        if len(e.command) < 2:
-            op = await skip_current_song(chat_id)
-            if op == 0:
-                await e.reply("**There's nothing in the queue to skip!**")
+    inp = e.text[5:]
+    chat_ = await vcbot.get_chat(inp)
+    chat_id = chat_.id
+    if len(e.command) < 2:
+        op = await skip_current_song(chat_id)
+        if op == 0:
+            await e.reply_text("**There's nothing in the queue to skip!**")
         elif op == 1:
-                await e.reply("**Empty Queue, Leaving Voice Chat**")
+            await e.reply_text("**Empty Queue, Leaving Voice Chat**")
         else:
-                await e.reply(
-                    f"**Playing In {chat_id}**",
-                    disable_web_page_preview=True,
-            )
+            await e.reply_text(f"**Playing In {chat_.title}**")
     else:
         skip = e.text.split(None, 1)[1]
         OP = "**Removed the following songs from the Queue: -**"
@@ -46,5 +38,5 @@ async def ping(_, e: Message):
                         pass
                     else:
                         OP = OP + "\n" + f"**#⃣{x}** - {hm}"
-            await e.reply(OP)        
+            await e.reply_text(OP)        
       
