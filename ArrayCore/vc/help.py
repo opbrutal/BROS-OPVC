70be1418a02e93450006f8f96f5553f854d64cd3
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 14:31:54 +0530

    Update help.py

--- ArrayCore/vc/help.py
+++ ArrayCore/vc/help.py
@@ -1,34 +1,34 @@
+import asyncio
 import os
 import sys
-import asyncio
-from pyrogram import Client
-from pyrogram import filters
-from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
-from time import time
+
 from datetime import datetime
-from var import Var
-from search import Client, Client2, Client3, Client4, Client5, Client6, Client7, Client8, Client9, Client10, Client11, Client12, Client13, Client14, Client15
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from time import time
 
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
+from search import (Venom1, Venom2, Venom3, Venom4,
+                    Venom5, Venom6, Venom7, Venom8,
+                    Venom9, Venom10, Venom11, Venom12,
+                    Venom13, Venom14, Venom15, HNDLR,
+                    call_py, contact_filter, SUDO_USERS)
 
-@Client.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client2.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client3.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client4.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client5.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client6.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client7.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client8.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client9.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client10.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client11.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client12.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client13.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client14.on(filters.command(["help"], prefixes=f"{HNDLR}"))
-@Client15.on(filters.command(["help"], prefixes=f"{HNDLR}"))
+
+@Venom1.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom2.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom3.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom4.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom5.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom6.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom7.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom8.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom9.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom10.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom11.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom12.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom13.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom14.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
+@Venom15.on_message(filters.command(["help"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
        HELP = f"""
