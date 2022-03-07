d351031e6db02a0c21da84f741b327eeff8e7ce9
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 16:53:15 +0530

    Update recruiting.py

--- ArrayCore/vc/recruiting.py
+++ ArrayCore/vc/recruiting.py
@@ -1,28 +1,18 @@
 import os
 import sys
-import asyncio
-from pyrogram import Client
+
 from pyrogram import filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
-from time import time
-from datetime import datetime
-from var import Var
+
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
                     HNDLR, call_py, contact_filter, SUDO_USERS)
-
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
     
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["restart"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-       await e.reply("`Restarting...`")
-       os.execl(sys.executable, sys.executable, *sys.argv)
-   # You probably don't need it but whatever
-       quit()
+    await e.reply_text("`Restarting...`")
+    os.execl(sys.executable, sys.executable, *sys.argv)
+    quit()
