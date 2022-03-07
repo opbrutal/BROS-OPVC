84c4336685113cc8708e8a877646aa3a42872105
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 22:43:06 +0530

    Update resume.py

--- ArrayCore/vc/resume.py
+++ ArrayCore/vc/resume.py
@@ -11,11 +11,9 @@ from search import (Venom1, Venom2, Venom3, Venom4,
                     HNDLR, call_py, contact_filter, SUDO_USERS)
 
 
-@vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["resume"], prefixes=HNDLR))
+@vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["resume"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    inp = e.text[5:]
-    chat_ = await vcbot.get_chat(inp)
-    chat_id = chat_.id
+    chat_id = e.chat.id
     if chat_id in QUEUE:
         try:
             await call_py.resume_stream(chat_id)
