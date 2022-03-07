960f2d6e46ddb7f75f9bd69a19af3f3eba5b9622
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 14:48:49 +0530

    Update end.py

--- ArrayCore/vc/end.py
+++ ArrayCore/vc/end.py
@@ -14,20 +14,6 @@ from search import (Venom1, Venom2, Venom3, Venom4,
 
 
 @Venom1.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom2.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom3.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom4.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom5.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom6.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom7.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom8.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom9.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom10.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom11.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom12.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom13.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom14.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
-@Venom15.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
 async def ping(_, e: Message):
     if e.from_user.id in SUDO_USERS:
         await e.delete()
@@ -41,3 +27,228 @@ async def ping(_, e: Message):
                 await e.reply(f"**ERROR** \n`{e}`")
         else:
             await e.reply("**Nothing is playing !**")
+
+if Venom2:
+  @Venom2.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom3:
+  @Venom3.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom4:
+  @Venom4.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom5:
+  @Venom5.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom6:
+  @Venom6.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom7:
+  @Venom7.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom8:
+  @Venom8.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom9:
+  @Venom9.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom10:
+  @Venom10.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom11:
+  @Venom11.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom12:
+  @Venom12.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom13:
+  @Venom13.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom14:
+  @Venom14.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
+if Venom15:
+  @Venom15.on_message(filters.command(["end"], prefixes=f"{HNDLR}"))
+  async def ping(_, e: Message):
+  if e.from_user.id in SUDO_USERS:
+    await e.delete()
+    chat_id = e.chat.id
+    if chat_id in QUEUE:
+      try:
+        await call_py.leave_group_call(chat_id)
+        clear_queue(chat_id)
+        await e.reply("**End**")
+      except Exception as e:
+        await e.reply(f"**ERROR** \n`{e}`")
+    else:
+      await e.reply("**Nothing is playing !**")
+
