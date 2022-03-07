93d723056bbb174a38354b706bd74ca28db7bd3b
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 15:51:34 +0530

    Update ping.py

--- ArrayCore/vc/ping.py
+++ ArrayCore/vc/ping.py
@@ -1,31 +1,46 @@
+import asyncio
 import os
 import sys
-import asyncio
-from pyrogram import Client
-from pyrogram import filters
+import datetime
+
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
 from time import time
-from datetime import datetime
-from var import Var
+
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+                    HNDLR, call_py, contact_filter, SUDO_USERS, start_time)
 
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
-    
+
+async def get_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+    hmm = len(time_list)
+    for x in range(hmm):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+    time_list.reverse()
+    up_time += ":".join(time_list)
+    return up_time
+
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["ping"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-        start = time()
-        current_time = datetime.utcnow()
-        m_reply = await e.reply_text("`Alive♤`")
-        delta_ping = time() - start
-        uptime_sec = (current_time - START_TIME).total_seconds()
-        uptime = await _human_time_duration(int(uptime_sec))
-        await m_reply.edit(f"Pong!")
+    st = datetime.datetime.now()
+    uptime = await get_time((time.time() - start_time))
+    x = await message.reply_text("**Ping !!**")
+    et = datetime.now()
+    pt = (et-st).microseconds / 1000
+    await x.edit_text(f"**Alive !!**\n\nPing: `{pt} ms` \nUptime: `{uptime}`")
