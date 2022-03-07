a3696eebf2f6a581d72e40ddcc7a85c0c1d2a9e6
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 14:11:59 +0530

    Update end.py

--- ArrayCore/vc/end.py
+++ ArrayCore/vc/end.py
@@ -1,18 +1,19 @@
-from pyrogram import Client as Venom
 import os
-from pyrogram import filters
+
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from config import bot, call_py, HNDLR, contact_filter
+
 from ArrayCore.vc.handlers import skip_current_song, skip_item
-from ArrayCore.vc.queues import QUEUE, clear_queue
-from var import Var
-from search import Venom2, Venom3, Venom5, Venom4, Venom6, Venom7, Venom8, Venom9, Venom10, Venom11, Venom12, Venom13, Venom14, Venom15
+from ArrayCore.vc.queues import clear_queue, QUEUE
+from search import (Venom1, Venom2, Venom3, Venom4,
+                    Venom5, Venom6, Venom7, Venom8,
+                    Venom9, Venom10, Venom11, Venom12,
+                    Venom13, Venom14, Venom15, HNDLR,
+                    vcbot, call_py, contact_filter, SUDO_USERS)
 
-SUDO_USERS = []
-for x in Var.SUDO_USERS: 
-    SUDO_USERS.append(x)
 
-@Venom.on(filters.command(["end"], prefixes=f"{HNDLR}"))
+
+@Venom1.on(filters.command(["end"], prefixes=f"{HNDLR}"))
 @Venom2.on(filters.command(["end"], prefixes=f"{HNDLR}"))
 @Venom3.on(filters.command(["end"], prefixes=f"{HNDLR}"))
 @Venom4.on(filters.command(["end"], prefixes=f"{HNDLR}"))
@@ -40,6 +41,3 @@ async def ping(_, e: Message):
                 await e.reply(f"**ERROR** \n`{e}`")
         else:
             await e.reply("**Nothing is playing !**")
-
-
-
