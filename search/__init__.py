ab39f3c2007fa0c92d5ba84cd79f380bfd46678e
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 12:54:20 +0530

    Update __init__.py

--- search/__init__.py
+++ search/__init__.py
@@ -44,91 +44,76 @@ GRPPLAY = grp
 
 
 if SESSION1:
-    print("Session 1 found")
     Venom1 = Client(SESSION1, api_id=API_ID, api_hash=API_HASH, plugins=dict(root="ArrayCore.vc"))
     call_py = PyTgCalls(Venom1)
 
 if SESSION2:
-    print("Session 2 found")
     Venom2 = Client(SESSION2, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom2 = None
 
 if SESSION3:
-    print("Session 3 found")
     Venom3 = Client(SESSION3, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom3 = None
 
 if SESSION4:
-    print("Session 4 found")
     Venom4 = Client(SESSION4, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom4 = None
 
 if SESSION5:
-    print("Session 5 found")
     Venom5 = Client(SESSION5, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom5 = None
 
 if SESSION6:
-    print("Session 6 found")
     Venom6 = Client(SESSION6, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom6 = None
         
 if SESSION7:
-    print("Session 7 found")
     Venom7 = Client(SESSION7, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom7 = None
 
 if SESSION8:
-    print("Session 8 found")
     Venom8 = Client(SESSION8, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom8 = None
 
 if SESSION9:
-    print("Session 9 found")
     Venom9 = Client(SESSION9, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom9 = None
     
 if SESSION10:
-    print("Session 10 found")
     Venom10 = Client(SESSION10, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom10 = None
            
 if SESSION11:
-    print("Session 11 found")
     Venom11 = Client(SESSION11, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom11 = None
 
 if SESSION12:
-    print("Session 12 found")
     Venom12 = Client(SESSION12, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom12 = None
 
 if SESSION13:
-    print("Session 13 found")
     Venom13 = Client(SESSION13, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom13 = None
 
 if SESSION14:
-    print("Session 14 found")
     Venom14 = Client(SESSION14, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom14 = None
 
 if SESSION15:
-    print("Session 15 found")
     Venom15 = Client(SESSION15, api_id=API_ID, api_hash=API_HASH)
 else:
     Venom15 = None
-   
+
