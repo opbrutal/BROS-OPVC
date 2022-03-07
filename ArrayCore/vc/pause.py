8e48b2ca72596b99dc4f6452f955aa6cbefa37d4
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-07 15:41:03 +0530

    Update pause.py

--- ArrayCore/vc/pause.py
+++ ArrayCore/vc/pause.py
@@ -6,37 +6,21 @@ from ArrayCore.vc.queues import QUEUE, clear_queue
 from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
-                    Venom13, Venom14, Venom15, HNDLR,
-                    call_py, contact_filter, SUDO_USERS)
+                    Venom13, Venom14, Venom15, vcbot,
+                    HNDLR, call_py, contact_filter, SUDO_USERS)
 
 
-@Venom1.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom2.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom3.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom4.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom5.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom6.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom7.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom8.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom9.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom10.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom11.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom12.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom13.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom14.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
-@Venom15.on_message(filters.command(["pause"], prefixes=f"{HNDLR}"))
+@vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["pause"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    if e.from_user.id in SUDO_USERS:
-        await e.delete()
-        chat_id = e.chat.id
-        if chat_id in QUEUE:
-            try:
-                await call_py.pause_stream(chat_id)
-                await e.reply(
-                    f"**Paued In {chat_id}**"
-                )
-            except Exception as e:
-                await e.reply(f"**ERROR** \n`{e}`")
-        else:
-            await e.reply("**Nothing is playing!**")
+    inp = e.text[5:]
+    chat_ = await vcbot.get_chat(inp)
+    chat_id = chat_.id
+    if chat_id in QUEUE:
+        try:
+            await call_py.pause_stream(chat_id)
+            await e.reply_text(f"**Paued In:** {chat_.title}")
+        except Exception as e:
+            await e.reply_text(f"**ERROR** \n`{e}`")
+    else:
+        await e.reply_text("**Nothing is playing!**")
       
