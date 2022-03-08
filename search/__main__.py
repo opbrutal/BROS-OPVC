90e313560318aee6399696de69ff2fc6fd49bf20
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:36:22 +0530

    Update __main__.py

--- search/__main__.py
+++ search/__main__.py
@@ -5,135 +5,141 @@ from pyrogram import idle
 from . import (Venom1, Venom2, Venom3, Venom4,
                Venom5, Venom6, Venom7, Venom8,
                Venom9, Venom10, Venom11, Venom12,
-               Venom13, Venom14, Venom15, vcbot, call_py)
+               Venom13, Venom14, Venom15, vcbot)
+from . import (call_py1, call_py2, call_py3, call_py4,
+               call_py5, call_py6, call_py7, call_py8,
+               call_py9, call_py10, call_py11, call_py12,
+               call_py13, call_py14, call_py15)
 
 
 async def startup():
-    print("••• Starting Clients •••")
+    # STARTING CLIENTS
     if Venom1:
         try:
             await Venom1.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 1 not found")
 
     if Venom2:
         try:
             await Venom2.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 2 not found")
 
     if Venom3:
         try:
             await Venom3.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 3 not found")
 
     if Venom4:
         try:
             await Venom4.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 4 not found")
 
     if Venom5:
         try:
             await Venom5.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 5 not found")
 
     if Venom6:
         try:
             await Venom6.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 6 not found")
 
     if Venom7:
         try:
             await Venom7.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 7 not found")
 
     if Venom8:
         try:
             await Venom8.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 8 not found")
 
     if Venom9:
         try:
             await Venom9.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 9 not found")
 
     if Venom10:
         try:
             await Venom10.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 10 not found")
 
     if Venom11:
         try:
             await Venom11.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 11 not found")
 
     if Venom12:
         try:
             await Venom12.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 12 not found")
 
     if Venom13:
         try:
             await Venom13.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 13 not found")
 
     if Venom14:
         try:
             await Venom14.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 14 not found")
 
     if Venom15:
         try:
             await Venom15.start()
         except Exception as e:
             print(str(e))
-    else:
-        print("Client 15 not found")
-    
-    await vcbot.start()
-    await call_py.start()
 
-    print("••• Bot Started •••")
+    # STARTING BOT CLIENT
+    await vcbot.start()
+
+    # STARTING ASSISTANTS
+    if call_py1:
+        await call_py1.start()
+    if call_py2:
+        await call_py2.start()
+    if call_py3:
+        await call_py3.start()
+    if call_py4:
+        await call_py4.start()
+    if call_py5:
+        await call_py5.start()
+    if call_py6:
+        await call_py6.start()
+    if call_py7:
+        await call_py7.start()
+    if call_py8:
+        await call_py8.start()
+    if call_py9:
+        await call_py9.start()
+    if call_py10:
+        await call_py10.start()
+    if call_py11:
+        await call_py11.start()
+    if call_py12:
+        await call_py12.start()
+    if call_py13:
+        await call_py13.start()
+    if call_py14:
+        await call_py14.start()
+    if call_py15:
+        await call_py15.start()
+    
+    # STARTUP COMPLETED
     await idle()
 
 
