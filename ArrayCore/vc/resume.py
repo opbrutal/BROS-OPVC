ff1cdf1073633d9105ccfaab1c69d4e319ebe498
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:49:57 +0530

    Update resume.py

--- ArrayCore/vc/resume.py
+++ ArrayCore/vc/resume.py
@@ -4,11 +4,7 @@ from pyrogram.types import Message
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import QUEUE, clear_queue
 
-from search import (Venom1, Venom2, Venom3, Venom4,
-                    Venom5, Venom6, Venom7, Venom8,
-                    Venom9, Venom10, Venom11, Venom12,
-                    Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+from search import vcbot, call_py1, HNDLR, SUDO_USERS)
 
 
 @vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["resume"], prefixes=HNDLR))
@@ -16,7 +12,7 @@ async def ping(_, e: Message):
     chat_id = e.chat.id
     if chat_id in QUEUE:
         try:
-            await call_py.resume_stream(chat_id)
+            await call_py1.resume_stream(chat_id)
             await e.reply_text(f"**Resumed In {chat_.title}**")
         except Exception as e:
             await e.reply_text(f"**ERROR** \n`{e}`")
