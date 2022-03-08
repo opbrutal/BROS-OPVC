da406176f15086071ba283e411082e8bbdc5f0f4
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:47:58 +0530

    Update playlist.py

--- ArrayCore/vc/playlist.py
+++ ArrayCore/vc/playlist.py
@@ -3,11 +3,7 @@ from pyrogram.types import Message
 
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import QUEUE, clear_queue
-from search import (Venom1, Venom2, Venom3, Venom4,
-                    Venom5, Venom6, Venom7, Venom8,
-                    Venom9, Venom10, Venom11, Venom12,
-                    Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+from search import vcbot, HNDLR, SUDO_USERS
     
 
 @vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["list"], prefixes=HNDLR))
