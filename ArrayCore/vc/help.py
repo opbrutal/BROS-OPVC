91d94ee4c6534598995eeb8ca1c9ace3e91ce023
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:42:05 +0530

    Update help.py

--- ArrayCore/vc/help.py
+++ ArrayCore/vc/help.py
@@ -7,11 +7,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from time import time
 
-from search import (Venom1, Venom2, Venom3, Venom4,
-                    Venom5, Venom6, Venom7, Venom8,
-                    Venom9, Venom10, Venom11, Venom12,
-                    Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+from search import vcbot, HNDLR, SUDO_USERS
 
 HELP = f"""Help Menu For VcRaid By [Akash](https/t.me/TheVenomXD).
 Use Your Command HNDLR To Use It I Am Giving ! As Default
