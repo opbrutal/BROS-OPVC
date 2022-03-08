906b6a7d068ab91f2fe913fe393125fe8fac5dda
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:48:52 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -4,11 +4,7 @@ import sys
 from pyrogram import filters
 from pyrogram.types import Message
 
-from search import (Venom1, Venom2, Venom3, Venom4,
-                    Venom5, Venom6, Venom7, Venom8,
-                    Venom9, Venom10, Venom11, Venom12,
-                    Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+from search import vcbot, HNDLR, SUDO_USERS
     
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=HNDLR))
