d66eb2b12662be7d6996a3b422b976735c0acc94
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-08 23:37:29 +0530

    Update end.py

--- ArrayCore/vc/end.py
+++ ArrayCore/vc/end.py
@@ -9,7 +9,11 @@ from search import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, vcbot,
-                    HNDLR, call_py, contact_filter, SUDO_USERS)
+                    HNDLR, contact_filter, SUDO_USERS)
+from search import (call_py1, call_py2, call_py3, call_py4,
+               call_py5, call_py6, call_py7, call_py8,
+               call_py9, call_py10, call_py11, call_py12,
+               call_py13, call_py14, call_py15)
 
 
 
@@ -18,7 +22,7 @@ async def ping(_, e: Message):
     chat_id = e.chat.id
     if chat_id in QUEUE:
         try:
-            await call_py.leave_group_call(chat_id)
+            await call_py1.leave_group_call(chat_id)
             clear_queue(chat_id)
             await e.reply_text("**End**")
         except Exception as ex:
