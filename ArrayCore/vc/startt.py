640cce4461e571f3009140cf0a852d135577dfca
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-10 23:37:08 +0530

    Update startt.py

--- ArrayCore/vc/startt.py
+++ ArrayCore/vc/startt.py
@@ -6,7 +6,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 from time import time
 from datetime import datetime
-from search import vcbot 
+from search import vcbot, SUDO_USERS 
 
 @vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["start"], prefixes=HNDLR))
 async def start(_, e: Message):
