96a64f87358a5f278c78d80996dcab2081c37f79
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-11 00:42:25 +0530

    Update startt.py

--- ArrayCore/vc/startt.py
+++ ArrayCore/vc/startt.py
@@ -10,10 +10,10 @@ from search import vcbot, SUDO_USERS, HNDLR
 
 @vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["start"], prefixes=HNDLR))
 async def start(_, e: Message):
-    await client.send_message(
+    await vcbot.send_message(
         text = f"""
 Vc Raid Bot Is Working Fine 
 Send !help To Know Your Commands
 Powered By @ArrayCore
-"""
+""")
         await e.reply(HELP)
