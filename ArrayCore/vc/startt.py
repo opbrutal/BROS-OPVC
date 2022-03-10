c614d4856f48c0f5a2d018060b8eb4283126bed1
Akash Tiwari <84653553+desinobita@users.noreply.github.com>
2022-03-10 23:15:34 +0530

    Add files via upload

---
+++ ArrayCore/vc/startt.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import asyncio
+from pyrogram import Client
+from pyrogram import filters
+from pyrogram.types import Message
+from config import bot, call_py, HNDLR, contact_filter
+from time import time
+from datetime import datetime
+
+SUDO_USERS = []
+for x in Var.SUDO_USERS: 
+    SUDO_USERS.append(x)
+
+@vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["start"], prefixes=HNDLR))async def ping(_, e: Message):
+async def start(_, e: Message):
+    if e.from_user.id in SUDO_USERS:
+    HELP = f"""
+Vc Raid Bot Is Working Fine 
+Send !help To Know Your Commands
+Powered By @ArrayCore
+"""
+   await e.reply(HELP)
