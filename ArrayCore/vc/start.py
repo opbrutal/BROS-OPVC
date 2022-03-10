ba1b22e3b29f3600a58c7d3dd5a79ed8f1121bae
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-11 01:30:09 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -6,4 +6,4 @@ from search import vcbot, SUDO_USERS, HNDLR, hl
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["start"], prefixes=HNDLR))
 async def start(_, e: Message):
-    await vcbot.send_message(f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
+    await vcbot.send_message(e.chat.id, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
