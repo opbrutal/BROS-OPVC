3fd6895e91c7fa985adb5f80cdd48dde79d7e657
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-12 00:56:34 +0530

    Update end.py

--- ArrayCore/vc/end.py
+++ ArrayCore/vc/end.py
@@ -5,7 +5,7 @@ from pyrogram.types import Message
 
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import clear_queue, QUEUE
-from ..utils import call_py1, vcbot, HNDLR, SUDO_USERS
+from .. import call_py1, vcbot, HNDLR, SUDO_USERS
 
 
 
