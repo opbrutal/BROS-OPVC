6730976ddeea4799de4638cb67f44b346446a478
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-12 00:55:51 +0530

    Update Alive.py

--- ArrayCore/vc/Alive.py
+++ ArrayCore/vc/Alive.py
@@ -1,4 +1,4 @@
-from ..utils import (Venom1, Venom2, Venom3, Venom4,
+from .. import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, HNDLR,
