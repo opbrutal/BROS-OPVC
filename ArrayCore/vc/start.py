d84a3a5c4ecd12d98b31ff0edd5e7cfecd1e0b79
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-11 23:57:22 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -8,7 +8,7 @@ from ..utils import vcbot, SUDO_USERS, HNDLR, hl, START_VID
 # @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["start"], prefixes=HNDLR))
 # async def start(_, e: Message):
    # video=START_VID,
-  $  await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
+  # await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
 
 
 @vcbot.on_message(command(["start"]))
