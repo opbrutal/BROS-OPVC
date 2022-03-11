3104ff32d53f9416b668bb726924bdedcdc2fbae
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-11 23:13:57 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -3,10 +3,9 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from search import vcbot, SUDO_USERS, HNDLR, hl
-from search import START_VID
+from ..utils import vcbot, SUDO_USERS, HNDLR, hl, START_VID
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.private & filters.command(["start"], prefixes=HNDLR))
 async def start(_, e: Message):
     video=START_VID,
-    await vcbot.send_message(e.chat.id, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
+    await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
