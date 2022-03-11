f671f9f1734e7075efad5f7d11723fc7361709c0
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-12 00:56:18 +0530

    Update chats.py

--- ArrayCore/vc/chats.py
+++ ArrayCore/vc/chats.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ..utils import (Venom1, Venom2, Venom3, Venom4,
+from .. import (Venom1, Venom2, Venom3, Venom4,
                     Venom5, Venom6, Venom7, Venom8,
                     Venom9, Venom10, Venom11, Venom12,
                     Venom13, Venom14, Venom15, HNDLR,
