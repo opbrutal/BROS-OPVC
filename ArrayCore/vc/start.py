63559c3da680a1a965caafee359c451567a5696a
The RiZoeL <85222112+MrRizoel@users.noreply.github.com>
2022-03-11 23:33:00 +0530

    Update start.py

--- ArrayCore/vc/start.py
+++ ArrayCore/vc/start.py
@@ -9,3 +9,29 @@ from ..utils import vcbot, SUDO_USERS, HNDLR, hl, START_VID
 async def start(_, e: Message):
     video=START_VID,
     await vcbot.send_video(e.chat.id, video, f"Vc Raid Bot Is Working Fine. \nSend `{hl}help` To Know Your Commands. \n\n< Powered By @ArrayCore >")
+
+
+@vcbot.on_message(command(["start"]))
+async def _start(_, ok: Message):
+    if ok.chat.type == "private":
+        await ok.reply_text(
+            f"**Hello {Message.from_user.mention} !** \n\n __ • I'm ArrayCore An Advance And Simple Group Voice Call Bot__ \n\n **Click Below Buttons for More Info**"
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                    InlineKeyboardButton(
+                        "• Channel •", url="https://t.me/ArrayCore"),
+                    InlineKeyboardButton(
+                        "• Support •", url="https://t.me/DNHxHELL")
+                ], [
+                    InlineKeyboardButton(
+                        "• Repo •", url="https://github.com/desinobita/TgVcUsers")
+                ]]
+            ))
+    else:
+        await ok.reply_text("**✨ ArrayCore is On ✨**",
+                           reply_markup=InlineKeyboardMarkup(
+                               [[
+                                   InlineKeyboardButton("• Channel •", url="https://t.me/ArrayCore")
+                               ]]
+                           )
+                           )
