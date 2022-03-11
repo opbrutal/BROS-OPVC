6258b7d6c973030d93855d832f64e1e808503486
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-11 23:12:37 +0530

    Update resume.py

--- ArrayCore/vc/resume.py
+++ ArrayCore/vc/resume.py
@@ -4,7 +4,7 @@ from pyrogram.types import Message
 from ArrayCore.vc.handlers import skip_current_song, skip_item
 from ArrayCore.vc.queues import QUEUE, clear_queue
 
-from search import vcbot, call_py1, HNDLR, SUDO_USERS
+from ..utils import vcbot, call_py1, HNDLR, SUDO_USERS
 
 
 @vcbot.on_message(filters.user(SUDO_USERS) & ~filters.private & filters.command(["resume"], prefixes=HNDLR))
