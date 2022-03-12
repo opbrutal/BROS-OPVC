7c0a414f31811851918b44bcf11de5e57db6f6f2
Hêllẞøy <72251918+HellBoy-OP@users.noreply.github.com>
2022-03-12 14:39:12 +0530

    Update vcraid.py

--- ArrayCore/vc/vcraid.py
+++ ArrayCore/vc/vcraid.py
@@ -261,7 +261,7 @@ async def vcraid(_, e: Message):
     if inp:
         TheVenomXD = await e.reply_text("**Starting VC raid**")
         link = f"https://itshellboy.tk/{aud[1:]}"
-        dl = aud
+        dl = await vcbot.download_audio(aud)
         songname = aud[18:]
         if chat_id in QUEUE:
             pos = add_to_queue(chat_id, songname, dl, link, "Audio", 0)
