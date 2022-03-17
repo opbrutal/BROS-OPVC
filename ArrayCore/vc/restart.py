a062af157afe0038a5bb7c33f264dfe2f50afb89
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-03-17 10:35:03 +0530

    Update restart.py

--- ArrayCore/vc/restart.py
+++ ArrayCore/vc/restart.py
@@ -9,6 +9,6 @@ from .. import vcbot, HNDLR, SUDO_USERS
 
 @vcbot.on_message(filters.user(SUDO_USERS) & filters.command(["restart"], prefixes=HNDLR))
 async def ping(_, e: Message):
-    await e.reply_text("`Restarting...`")
+    await e.reply_text("`ʀᴇsᴛᴀʀᴛɪɴɢ...`")
     os.execl(sys.executable, sys.executable, *sys.argv)
     quit()
